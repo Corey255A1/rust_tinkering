06e32999ecb2dae769a0ec0ff2d66568d196c760
corey255a1 <wundervision2013@gmail.com>
2024-06-15 14:10:53 -0400

    Loops and vectors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 // How to import other "crates"
 use std::{
     io::{self, Write},
-    process::exit,
+    process::exit, vec,
 };
 
 pub trait NumberProperties {
@@ -56,8 +56,17 @@ fn main() {
     }
 
     collatz(resulting_int);
+    let mut collatz_collection: Vec<i32> = Vec::new();
+
+    collatz_loop(resulting_int, &mut collatz_collection);
+
+    for result in collatz_collection {
+        println!("{}", result);
+    }
+
 }
 
+// Recursively calling and printing the collatz sequence
 fn collatz(n: i32) -> i32 {
     println!("{}", n);
     match n {
@@ -65,5 +74,17 @@ fn collatz(n: i32) -> i32 {
         // using a trait to replace this if x & 1 == 1 => return collatz(3 * n + 1),
         x if x.is_odd() => return collatz(3 * n + 1),
         _ => return collatz(n / 2),
+    }
+}
+
+// Passing in a mutable vector and filling it with the results of the collatz loop
+fn collatz_loop(n:i32, result:&mut Vec<i32>) {
+    let mut working = n;
+    while working != 1 {
+        working = match working {
+            x if x.is_odd() => 3 * x + 1,
+            _ => working / 2,
+        };
+        result.push(working);
     }
 }
